1405a07309f8e87e5c2ab7ed8bbbfe365ba87dd7
Saipreethamgit <nsaipreetham000@gmail.com>
2025-08-06 19:37:20 -0500

    Update Product.java

--- src/main/java/com/gamercein/backend/model/Product.java
+++ src/main/java/com/gamercein/backend/model/Product.java
@@ -3,7 +3,7 @@ package com.gamercein.backend.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "products")
+@Document(collection = "gamercein")
 public class Product {
 
     @Id
