f90a5cb1da256391dfbe6fe7ce8c1f1aabf9fef0
Saipreethamgit <nsaipreetham000@gmail.com>
2025-08-13 12:40:57 -0500

    Update WebConfig.java

--- src/main/java/com/gamercein/backend/config/WebConfig.java
+++ src/main/java/com/gamercein/backend/config/WebConfig.java
@@ -14,7 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")  // your React app URL
+                        .allowedOrigins(
+    "http://localhost:3000", 
+    "*"
+)
+  // your React app URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
