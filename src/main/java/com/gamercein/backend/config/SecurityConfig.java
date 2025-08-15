797e9bfd68c6f182883847adfde3887e1ffed45c
Saipreethamgit <nsaipreetham000@gmail.com>
2025-08-15 00:10:01 -0500

    Update SecurityConfig.java

--- src/main/java/com/gamercein/backend/config/SecurityConfig.java
+++ src/main/java/com/gamercein/backend/config/SecurityConfig.java
@@ -26,12 +26,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
 }
+
