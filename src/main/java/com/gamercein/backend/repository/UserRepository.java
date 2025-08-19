7ee4d9383baf12bf96d9467c7a2c9bf8c9f909fd
Saipreethamgit <nsaipreetham000@gmail.com>
2025-08-19 10:58:41 -0500

    Update UserRepository.java

--- src/main/java/com/gamercein/backend/repository/UserRepository.java
+++ src/main/java/com/gamercein/backend/repository/UserRepository.java
@@ -2,11 +2,13 @@ package com.gamercein.backend.repository;
 
 import com.gamercein.backend.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
- 
     Optional<User> findByEmail(String email);
-    
+    Optional<User> findByUsername(String username);
+
     boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
